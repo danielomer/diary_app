df250129333af46af42e8b087c71a08e8f01f499
danielomer <danialomer000@gmail.com>
2024-01-26 17:14:17 +0300

    feat: add sign up screen with validation and firebase connection

--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -9,22 +9,30 @@ class SignUpScreen extends StatefulWidget {
 }
 
 class _SignUpScreenState extends State<SignUpScreen> {
+  // initialize variables to store user input data
   String userName = '';
   String userEmail = '';
   String userPassword = '';
 
+  // create a global key to identify the form and validate the form
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  // create a function to get the user input data
   getUserName(name) {
     userName = name;
   }
 
+  //  create a function to get the user input data
   getUserEmail(email) {
     userEmail = email;
   }
 
+  //  create a function to get the user input data
   getUserPassword(password) {
     userPassword = password;
   }
 
+  // create a function to insert data into firebase
   insertData() {
     DocumentReference documentReference =
         FirebaseFirestore.instance.collection("users").doc();
@@ -39,52 +47,152 @@ class _SignUpScreenState extends State<SignUpScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // add overflow to avoid pixel overflow
+      resizeToAvoidBottomInset: false,
+
+      // app bar
       appBar: AppBar(
-        title: const Text('Sign Up'),
+        backgroundColor: Colors.purple,
+        title: const Text(
+          'Sign Up',
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
       ),
+
+      // body
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            TextField(
-              decoration: const InputDecoration(
-                labelText: 'Username',
+        padding: const EdgeInsets.symmetric(horizontal: 25),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // welcome text
+              const Text(
+                "Welcome to Diary App",
+                style: TextStyle(fontSize: 20),
               ),
-              keyboardType: TextInputType.name,
-              onChanged: (String name) {
-                getUserName(name);
-              },
-            ),
-            const SizedBox(height: 16.0),
-            TextField(
-              decoration: const InputDecoration(
-                labelText: 'Email',
+              const SizedBox(height: 25),
+
+              // form fields
+              TextFormField(
+                decoration: const InputDecoration(
+                  hintText: "Username",
+                  prefixIcon: Icon(
+                    Icons.person,
+                    color: Colors.black,
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                  ),
+                ),
+                keyboardType: TextInputType.name,
+                onChanged: (String name) {
+                  getUserName(name);
+                },
+                validator: (value) {
+                  if (value == null || value.isEmpty || value.length < 3) {
+                    return 'Username must be at least 3 characters';
+                  }
+                  return null; // Return null if the input is valid
+                },
               ),
-              keyboardType: TextInputType.emailAddress,
-              onChanged: (String email) {
-                getUserEmail(email);
-              },
-            ),
-            const SizedBox(height: 16.0),
-            TextField(
-              decoration: const InputDecoration(
-                labelText: 'Password',
+              const SizedBox(height: 16.0),
+              TextFormField(
+                decoration: const InputDecoration(
+                  hintText: "Email",
+                  prefixIcon: Icon(
+                    Icons.email,
+                    color: Colors.black,
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                  ),
+                ),
+                keyboardType: TextInputType.emailAddress,
+                onChanged: (String email) {
+                  getUserEmail(email);
+                },
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your email';
+                  }
+                  // Checking if the entered email has the right format
+                  if (!RegExp("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+.[a-z]")
+                      .hasMatch(value)) {
+                    return 'Please enter a valid email Address';
+                  }
+                  return null; // Return null if the input is valid
+                },
               ),
-              keyboardType: TextInputType.visiblePassword,
-              onChanged: (String password) {
-                getUserPassword(password);
-              },
-              obscureText: true,
-            ),
-            const SizedBox(height: 16.0),
-            ElevatedButton(
-              onPressed: () {
-                insertData();
-              },
-              child: const Text('Sign Up'),
-            ),
-          ],
+              const SizedBox(height: 16.0),
+              TextFormField(
+                decoration: const InputDecoration(
+                  hintText: "Password",
+                  prefixIcon: Icon(
+                    Icons.lock,
+                    color: Colors.black,
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                  ),
+                ),
+                keyboardType: TextInputType.visiblePassword,
+                onChanged: (String password) {
+                  getUserPassword(password);
+                },
+                validator: (value) {
+                  if (value == null || value.isEmpty || value.length < 6) {
+                    return 'Password must be at least 6 characters';
+                  }
+                  return null; // Return null if the input is valid
+                },
+                obscureText: true,
+              ),
+              const SizedBox(height: 16.0),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  fixedSize: const Size(150, 50),
+                  textStyle: const TextStyle(fontSize: 20),
+                  backgroundColor: Colors.purple,
+                  foregroundColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                ),
+                onPressed: () {
+                  if (!_formKey.currentState!.validate()) {
+                  } else {
+                    insertData();
+                  }
+                },
+                child: const Text('Sign Up'),
+              ),
+              const SizedBox(height: 25),
+
+              // sign in text if user already has an account
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text("Already have an account?"),
+                  const SizedBox(width: 5.0),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                    child: const Text(
+                      "Sign In",
+                      style: TextStyle(
+                        color: Colors.purple,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
