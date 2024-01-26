60e777ec12952e1a37091b12a797f1ab8d2e71d2
danielomer <danialomer000@gmail.com>
2024-01-26 23:56:13 +0300

    fix: signup create auth in firebase and add user detail for fire base document

--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -1,4 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:diary_app/screens/home_screen.dart';
+import 'package:diary_app/screens/signin_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class SignUpScreen extends StatefulWidget {
@@ -19,37 +22,49 @@ class _SignUpScreenState extends State<SignUpScreen> {
 
   bool _isLoading = false; // Loading state variable
 
-  // create a function to get the user input data
-  getUserName(name) {
-    userName = name;
-  }
-
-  //  create a function to get the user input data
-  getUserEmail(email) {
-    userEmail = email;
-  }
-
-  //  create a function to get the user input data
-  getUserPassword(password) {
-    userPassword = password;
-  }
+  final userNameController = TextEditingController();
+  final userEmailController = TextEditingController();
+  final userPasswordController = TextEditingController();
 
   // create a function to insert data into firebase
-  insertData() async {
+  void insertData() async {
     setState(() => _isLoading = true); // Start loading
 
-    DocumentReference documentReference =
-        FirebaseFirestore.instance.collection("users").doc();
-    Map<String, dynamic> studentData = {
-      "userName": userName,
-      "userEmail": userEmail,
-      "userPassword": userPassword
-    };
-    documentReference.set(studentData).whenComplete(() {
-      Future.delayed(const Duration(seconds: 1), () {
-        setState(() => _isLoading = false); // Stop loading
+    // if the form is valid then proceed
+    if (_formKey.currentState!.validate()) {
+      await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(
+            email: userEmailController.text,
+            password: userPasswordController.text,
+          )
+          .then((value) => addUserDetails())
+          .catchError((error) {
+        print('Failed to add user: $error');
+        setState(() => _isLoading = false);
       });
-    });
+    } else {
+      setState(() => _isLoading = false);
+    }
+  }
+
+  Future addUserDetails() async {
+    CollectionReference users = FirebaseFirestore.instance.collection('users');
+
+    return await users
+        .add({
+          'userName': userNameController.text,
+          'userEmail': userEmailController.text,
+          'userPassword': userPasswordController.text,
+        })
+        .then(
+          (value) => Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(
+              builder: (context) => HomeScreen(),
+            ),
+          ),
+        )
+        .catchError((error) => print('Failed to add user: $error'));
   }
 
   @override
@@ -68,141 +83,155 @@ class _SignUpScreenState extends State<SignUpScreen> {
       ),
 
       // body
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 25),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              // welcome text
-              const Text(
-                "Welcome to Diary App",
-                style: TextStyle(fontSize: 20),
-              ),
-              const SizedBox(height: 25),
+      body: SafeArea(
+        child: Center(
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    // welcome text
+                    const Text(
+                      "Welcome to Diary App",
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    const SizedBox(height: 25),
 
-              // form fields
-              TextFormField(
-                decoration: const InputDecoration(
-                  hintText: "Username",
-                  prefixIcon: Icon(
-                    Icons.person,
-                    color: Colors.black,
-                  ),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                  ),
-                ),
-                keyboardType: TextInputType.name,
-                onChanged: (String name) {
-                  getUserName(name);
-                },
-                validator: (value) {
-                  if (value == null || value.isEmpty || value.length < 3) {
-                    return 'Username must be at least 3 characters';
-                  }
-                  return null; // Return null if the input is valid
-                },
-              ),
-              const SizedBox(height: 16.0),
-              TextFormField(
-                decoration: const InputDecoration(
-                  hintText: "Email",
-                  prefixIcon: Icon(
-                    Icons.email,
-                    color: Colors.black,
-                  ),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                  ),
-                ),
-                keyboardType: TextInputType.emailAddress,
-                onChanged: (String email) {
-                  getUserEmail(email);
-                },
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter your email';
-                  }
-                  // Checking if the entered email has the right format
-                  if (!RegExp("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+.[a-z]")
-                      .hasMatch(value)) {
-                    return 'Please enter a valid email Address';
-                  }
-                  return null; // Return null if the input is valid
-                },
-              ),
-              const SizedBox(height: 16.0),
-              TextFormField(
-                decoration: const InputDecoration(
-                  hintText: "Password",
-                  prefixIcon: Icon(
-                    Icons.lock,
-                    color: Colors.black,
-                  ),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                  ),
-                ),
-                keyboardType: TextInputType.visiblePassword,
-                onChanged: (String password) {
-                  getUserPassword(password);
-                },
-                validator: (value) {
-                  if (value == null || value.isEmpty || value.length < 6) {
-                    return 'Password must be at least 6 characters';
-                  }
-                  return null; // Return null if the input is valid
-                },
-                obscureText: true,
-              ),
-              const SizedBox(height: 16.0),
-              ElevatedButton(
-                onPressed: _isLoading ? null : insertData,
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.purple,
-                  foregroundColor: Colors.white,
-                  minimumSize: const Size(double.infinity, 50),
-                ),
-                // if loading is true show loader else show text
-                child: _isLoading
-                    ? const SizedBox(
-                        width: 24,
-                        height: 24,
-                        child: CircularProgressIndicator(
-                          strokeWidth: 3, // stroke of the loader
-                          valueColor: AlwaysStoppedAnimation<Color>(
-                            Colors.white, // loader color
+                    // form fields
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        hintText: "Username",
+                        prefixIcon: Icon(
+                          Icons.person,
+                          color: Colors.black,
+                        ),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(10.0),
                           ),
                         ),
-                      )
-                    : const Text('Sign Up'),
-              ),
-              const SizedBox(height: 25),
-
-              // sign in text if user already has an account
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text("Already have an account?"),
-                  const SizedBox(width: 5.0),
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.pop(context);
-                    },
-                    child: const Text(
-                      "Sign In",
-                      style: TextStyle(
-                        color: Colors.purple,
-                        fontWeight: FontWeight.bold,
                       ),
+                      keyboardType: TextInputType.name,
+                      controller: userNameController,
+                      validator: (value) {
+                        if (value == null ||
+                            value.isEmpty ||
+                            value.length < 3) {
+                          return 'Username must be at least 3 characters';
+                        }
+                        return null; // Return null if the input is valid
+                      },
                     ),
-                  ),
-                ],
+                    const SizedBox(height: 16.0),
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        hintText: "Email",
+                        prefixIcon: Icon(
+                          Icons.email,
+                          color: Colors.black,
+                        ),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                        ),
+                      ),
+                      keyboardType: TextInputType.emailAddress,
+                      controller: userEmailController,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter your email';
+                        }
+                        // Checking if the entered email has the right format
+                        if (!RegExp("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+.[a-z]")
+                            .hasMatch(value)) {
+                          return 'Please enter a valid email Address';
+                        }
+                        return null; // Return null if the input is valid
+                      },
+                    ),
+                    const SizedBox(height: 16.0),
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        hintText: "Password",
+                        prefixIcon: Icon(
+                          Icons.lock,
+                          color: Colors.black,
+                        ),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                          // change border color
+                          borderSide: BorderSide(color: Colors.purple),
+                        ),
+                      ),
+                      keyboardType: TextInputType.visiblePassword,
+                      controller: userPasswordController,
+                      validator: (value) {
+                        if (value == null ||
+                            value.isEmpty ||
+                            value.length < 6) {
+                          return 'Password must be at least 6 characters';
+                        }
+                        return null; // Return null if the input is valid
+                      },
+                      obscureText: true,
+                    ),
+                    const SizedBox(height: 16.0),
+                    ElevatedButton(
+                      onPressed: _isLoading ? null : insertData,
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.purple,
+                        foregroundColor: Colors.white,
+                        side: const BorderSide(color: Colors.purple),
+                        minimumSize: const Size(double.infinity, 50),
+                      ),
+                      // if loading is true show loader else show text
+                      child: _isLoading
+                          ? const SizedBox(
+                              width: 24,
+                              height: 24,
+                              child: CircularProgressIndicator(
+                                strokeWidth: 3, // stroke of the loader
+                                valueColor: AlwaysStoppedAnimation<Color>(
+                                  Colors.white, // loader color
+                                ),
+                              ),
+                            )
+                          : const Text('Sign Up'),
+                    ),
+                    const SizedBox(height: 25),
+
+                    // sign in text if user already has an account
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text("Already have an account?"),
+                        const SizedBox(width: 5.0),
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => const SignInScreen(),
+                              ),
+                            );
+                          },
+                          child: const Text(
+                            "Sign In",
+                            style: TextStyle(
+                              color: Colors.purple,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
-            ],
+            ),
           ),
         ),
       ),
