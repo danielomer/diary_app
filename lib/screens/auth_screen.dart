45f40e8647c700113053c60f21edf86e60031c52
danielomer <danialomer000@gmail.com>
2024-01-26 21:19:46 +0300

    feat: add auth page to check if user logged in or not

---
+++ lib/screens/auth_screen.dart
@@ -0,0 +1,24 @@
+import 'package:diary_app/screens/home_screen.dart';
+import 'package:diary_app/screens/signin_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class AuthPage extends StatelessWidget {
+  const AuthPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return HomeScreen();
+          } else {
+            return const SignInScreen();
+          }
+        },
+      ),
+    );
+  }
+}
