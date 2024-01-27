8defae67da4333a98f6bef2bd6061856a9849481
danielomer <danialomer000@gmail.com>
2024-01-27 17:11:18 +0300

    feat: add diaryContainer component in home screen (static for now)

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -26,18 +26,105 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Home Screen'),
+        title: Row(
+          // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: const [
+            Icon(Icons.book, color: Colors.white),
+            SizedBox(width: 10),
+            Text('Diaries', style: TextStyle(color: Colors.white)),
+          ],
+        ),
+        backgroundColor: Colors.purple,
         actions: [
           IconButton(
             onPressed: signOut,
             icon: const Icon(
               Icons.logout,
+              color: Colors.white,
             ),
           ),
         ],
       ),
-      body: const Center(
-        child: Text('Welcome to the Home Screen!'),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              diaryContainer(),
+              diaryContainer(),
+              diaryContainer(),
+              diaryContainer(),
+              diaryContainer(),
+              diaryContainer(),
+              diaryContainer(),
+              diaryContainer(),
+              diaryContainer(),
+              diaryContainer(),
+              diaryContainer(),
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add, color: Colors.deepPurple),
+      ),
+    );
+  }
+}
+
+class diaryContainer extends StatelessWidget {
+  const diaryContainer({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(10),
+      padding: const EdgeInsets.all(10),
+      width: double.infinity,
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.purple),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          const Text(
+            'Diary 1',
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(height: 10),
+          const Text(
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+            maxLines: 1,
+            overflow: TextOverflow.ellipsis,
+            softWrap: false,
+            style: TextStyle(
+              fontSize: 16,
+            ),
+          ),
+          const SizedBox(height: 10),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Text(
+                '12/12/2021',
+                style: TextStyle(
+                  fontSize: 16,
+                ),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.delete),
+              ),
+            ],
+          )
+        ],
       ),
     );
   }
